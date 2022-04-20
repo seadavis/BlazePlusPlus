86f4378959a97b0ed5d5c3be40f6a228acd69eb0
seadavis <seadav.17@gmail.com>
2022-04-19 19:59:26 -0700

    Getting basic header file to work

--- src/main.cpp
+++ src/main.cpp
@@ -3,48 +3,13 @@
 #include <opencv2/highgui.hpp>
 #include <iostream>
 #include <stdio.h>
+#include <test_lib.h>
 
 using namespace cv;
 using namespace std;
+using namespace test_lib;
+
 int main(int argc, char* argv[])
 {
-    Mat frame;
-    //--- INITIALIZE VIDEOCAPTURE
-   
-    // open the default camera using default API
-    // cap.open(0);
-    // OR advance usage: select any API backend
-    int deviceID = 0;             // 0 = open default camera
-    int apiID = cv::CAP_ANY;      // 0 = autodetect default API
-    // open selected camera using selected API
-
-    cout << getBuildInformation();
-
-    cout << "Using File " << argv[1] << endl;
-    VideoCapture cap(argv[1]);
-    // check if we succeeded
-    
-    //--- GRAB AND WRITE LOOP
-    cout << "Start grabbing" << endl
-        << "Press any key to terminate" << endl;
-    for (;;)
-    {
-        // wait for a new frame from camera and store it into 'frame'
-        cap.read(frame);
-        // check if we succeeded
-        if (frame.empty()) {
-            cerr << "ERROR! blank frame grabbed\n";
-            break;
-        }
-        // show live and wait for a key with timeout long enough to show images
-        
-      
-        imshow("Live", frame);
-    
-
-        if (waitKey(5) >= 0)
-            break;
-    }
-    // the camera will be deinitialized automatically in VideoCapture destructor
-    return 0;
+    MyClass::hello_world();
 }
